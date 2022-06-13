75e6129c42cc6312112d04609e91ef6632257810
LinGaOscar <oscar987123@yahoo.com.tw>
2022-06-13 16:21:03 +0800

    reset entity

--- Issac/src/main/java/com/oscar/issac/domain/Book.java
+++ Issac/src/main/java/com/oscar/issac/domain/Book.java
@@ -2,16 +2,14 @@ package com.oscar.issac.domain;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "book")
 public class Book {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
     private String author;
